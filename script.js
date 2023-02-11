3f2fa5cd70bb7e83a8a7cdcf87baebef8e92f1d3
FireLiquidator <mrahulrahi@gmail.com>
2023-02-11 19:09:31 +0530

    commit

--- script.js
+++ script.js
@@ -95,3 +95,66 @@ class ResponsiveCards extends HTMLElement {
   }
 }
 customElements.define("responsive-cards", ResponsiveCards);
+
+class NFTCard extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="container">
+
+    <article class="main-image">
+      <img src="images/image-equilibrium.jpg" alt="">
+      <div>
+        <img src="images/icon-view.svg" alt="">
+      </div>
+    </article>
+
+    <article>
+      <h2>Equilibrium #3429</h2>
+      <p>Our Equilibrium collection promotes balance and calm.</p>
+
+      <ul>
+        <li><img src="images/icon-ethereum.svg" alt=""> 0.041 ETH</li>
+        <li><img src="images/icon-clock.svg" alt="">3 days left</li>
+      </ul>
+    </article>
+
+    <article class="avatar">
+      <img src="images/image-avatar.png" alt="">
+      <p>Creation of <span>Jules Wyvern</span></p>
+    </article>
+  </div>
+
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noopener"
+      target="_blank">Frontend Mentor</a>.
+    Coded by <a href="https://github.com/mrahulrahi" rel="noopener"
+      target="_blank">mrahulrahi</a>.
+  </div>
+  `;
+  }
+}
+customElements.define("nft-card", NFTCard);
+
+class QRCard extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="container">
+    <article>
+      <img src="images/image-qr-code.png" alt="">
+    </article>
+
+    <article>
+      <h1>Improve your front-end skills by building projects</h1>
+      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </article>
+  </div>
+  
+  
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noopener" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="https://github.com/mrahulrahi" rel="noopener" target="_blank">mrahulrahi</a>.
+  </div>
+  `;
+  }
+}
+customElements.define("qr-card", QRCard);
