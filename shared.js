3a1a33c53d7a9e8ffbda97ab65c0439f859b2e61
FireLiquidator <mrahulrahi@gmail.com>
2023-02-23 10:49:33 +0530

    commit

--- shared.js
+++ shared.js
@@ -18,7 +18,6 @@ var modal = document.querySelector(".modal");
 var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
-var mobileNav = document.querySelector(".mobile-nav");
 
 // console.dir(backdrop.style['background-image']);
 
