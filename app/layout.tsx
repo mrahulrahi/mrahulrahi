aed515386dd2cfe0cdf90d7a16476298b3b31fb4
mrahulrahi <mrahulrahi@gmail.com>
2023-11-19 00:13:11 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next'
 import 'bootstrap/dist/css/bootstrap.css'
 import ImportBsJS from "./importBsJS";
-import { Inter } from 'next/font/google'
+import { Josefin_Sans } from 'next/font/google'
 import './globals.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
-const inter = Inter({ subsets: ['latin'] })
+const josefinSans = Josefin_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'mrahulrahi',
@@ -22,11 +22,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-         <script src="https://kit.fontawesome.com/8c25ccfa63.js" crossorigin="anonymous"></script>
-       <ImportBsJS />
-      <Header />
-      <body className={inter.className}>{children}</body>
-      <Footer />
+      <script src="https://kit.fontawesome.com/8c25ccfa63.js" crossorigin="anonymous"></script>
+      <ImportBsJS />
+      <body className={josefinSans.className}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
 import Image from 'next/image'
-import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-    
-    </main>
+    <main >
+
+      <div className="hero-container d-flex align-items-start position-relative">
+        <div className="hero-bg" style={{ backgroundImage: "url(" + `${require("/public/hero-img.jpg")}` + ")", }}></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="hero-box d-flex align-items-center justify-content-center">
+                <div className="hero-text text-center">
+                  <h4><span>Hello,</span> I'm</h4>
+                  <h1>Rahul <span>Maurya</span></h1>
+                  <h3>I'm a <span>Web Developer</span>.</h3>
+                  <div className="hero-btn">
+                    <a href="#projects" className="btn btn-default">Featured Work
+                      <i className="fas fa-arrow-down ms-2"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </main >
   )
 }
