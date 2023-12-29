105e74e8bcc2d62e9e6655b3787b15519d8005a0
mrahulrahi <mrahulrahi@gmail.com>
2023-12-29 21:55:09 +0530

    container and heading added

---
+++ app/aerocPage/components/Heading/Heading.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Heading = () => {
+    return (
+        <>
+            <div className="heading d-flex justify-content-between align-items-end">
+                <h3>Projects</h3>
+               
+            </div>
+        </>
+    )
+}
+
+export default Heading
---
+++ app/components/Container/Container.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Container = () => {
+    return (
+        <>
+            <div className="content-container bg-green">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default Container
