b98caf11d8ef4b77960ab4806a7142d9e69c6b2d
mrahulrahi <mrahulrahi@gmail.com>
2024-03-01 22:50:36 +0530

    change in footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import './Footer.css'
 import { FaLinkedinIn, FaGithub, FaYoutube, FaTelegram } from "react-icons/fa";
-import Button from '../Button';
 
 const Footer = () => {
 
